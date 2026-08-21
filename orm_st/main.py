babc591d02c1f501d15b394a47738438315acc1f
Sergio <sergio.cerros@gmail.com>
2026-08-21 08:40:04 +0200

    [IMP] Implemented basic metaclass 'ModelMeta' defined intermediate class 'Model'

--- orm_st/main.py
+++ orm_st/main.py
@@ -1,13 +1,34 @@
+# Metaclass used so the object class can discover its own attributes (fields)
+# Not really necessary for a light ORM per se, but it offers scalability
+class ModelMeta(type):
+    def __new__(mcls, name, bases, attrs):
+        fields = {}
+
+        for key, value in attrs.items():
+            if isinstance(value, Field):
+                fields[key]=value
+        cls = super().__new__(mcls, name, bases, attrs)
+        cls.__fields__ = fields
+
+        return cls
+
+# Model class will be an ORM operations provider get_by_id, get_all, delete, save, etc...
+class Model(metaclass=ModelMeta):
+    pass
+
 class Field:
     field_type = None
     sql_type = None
 
-    def __init__(self, required=False, default=None):
+    def __init__(self, required=False, default=None, column = None):
         self.required = required
         self.default = default
+        self.column = column
 
     def __set_name__(self, owner, name):
         self.name = name
+        if self.column is None:     #Separating database name from class name
+            self.column = name
 
     def __get__(self, instance, owner=None):
         if instance is None:
@@ -15,16 +36,19 @@ class Field:
         return instance.__dict__.__get__(self.name)
 
     def __set__(self, instance, value):
+        # Check if accepts nullable (None -> required)
+        if value is None and self.required:
+            raise TypeError(f"Field is required. <{self.name}> cannot be None")
 
-        # Added field type validation
+        # Field type validation -> Python side
         if not isinstance(value, self.field_type):
             raise TypeError(
                 f"{self.name} must be of type {self.field_type.__name__}"
             )
         # End: field type validation
 
-        instance.__dict__[
-            self.name] = value  # A dictionary or other mapping object used to store an object's (writable) attributes.
+        # A dictionary or other mapping object used to store an object's (writable) attributes.
+        instance.__dict__[self.name] = value
 
     def __delete__(self, instance):
         pass
@@ -35,7 +59,7 @@ class IntField(Field):
     sql_type = "INTEGER"
 
     def __init__(self, primary_key=False, required=False, default=None):
-        super().__init__(required=required, default=default)
+        super().__init__(required=required, default=default, column = None)
         self.primary_key = primary_key
 
 
@@ -52,7 +76,7 @@ class CharField(Field):
     sql_type = "VARCHAR"
 
     def __init__(self, size, primary_key=False, required=False, default=None):
-        super().__init__(required=required, default=default)
+        super().__init__(required=required, default=default, column = None)
         self.primary_key = primary_key
         self.size = size
 
@@ -65,5 +89,3 @@ class BoolField(Field):
     #     super().__init__(required=required, default=default)
 
 
-class Lightorm:
-    pass
